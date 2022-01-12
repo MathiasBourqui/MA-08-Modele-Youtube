a54d2153f67662a0adb5559efdc170641e968fe4
GERMAIN Paul-Loup <Paul-Loup.GERMAIN@cpnv.ch>
2022-01-12 16:12:58 +0100

    Create data base Moukitube

--- Rendu/CreateDatabase MoukiTube.sql
+++ Rendu/CreateDatabase MoukiTube.sql
@@ -1,116 +1,191 @@
-USE [master]
-GO
+/*
+	Auteur : Paul-Loup Germain & Mathias Bourqui
+	Date : 12/01/2022
+	Projet : Création du fichier permettant de créer la base de donnée de Moukitube
+*/
 
-/****** Object:  Database [MoukiTube]    Script Date: 21.12.2021 14:14:58 ******/
-CREATE DATABASE [MoukiTube]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'Mouk
-iTube', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER_PLG\MSSQL\DATA\MoukiTube.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'MoukiTube_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER_PLG\MSSQL\DATA\MoukiTube_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
+USE master;
 GO
+	DROP DATABASE IF exists Moukitube
+	CREATE DATABASE Moukitube
+		ON
+		(NAME = Moukitube_data ,
+		FILENAME = 'C:\Moukitube.mdf',
+		SIZE = 20MB,
+		MAXSIZE = 50MB,
+		FILEGROWTH = 1MB)
+	LOG ON 
+		(NAME = Moukitube_log,
+		FILENAME = 'C:\Moukitube.ldf',
+		SIZE = 10MB,
+		MAXSIZE = 20MB,
+		FILEGROWTH = 1MB);
 
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [MoukiTube].[dbo].[sp_fulltext_database] @action = 'enable'
-end
 GO
+	DROP TABLE IF exists Moukitube.dbo.subscriptions
+	Create table Moukitube.dbo.subscriptions(
+		id INT NOT NULL,
+		CONSTRAINT PK_subscriptions_id PRIMARY KEY CLUSTERED (id),
+		types VARCHAR(100) NOT NULL,
+		CONSTRAINT Uniqueplace_subscriptions UNIQUE (types)
+	)
 
-ALTER DATABASE [MoukiTube] SET ANSI_NULL_DEFAULT OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.monetization
+	Create table Moukitube.dbo.monetization(
+		id INT NOT NULL,
+		CONSTRAINT PK_monetization_id PRIMARY KEY CLUSTERED (id),
+		date DATE NOT NULL,
+		amount FLOAT NOT NULL,
+		CONSTRAINT Uniqueplace_monetization UNIQUE (date, amount)
+	)
 
-ALTER DATABASE [MoukiTube] SET ANSI_NULLS OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.channel
+	Create table Moukitube.dbo.channel(
+		id INT NOT NULL,
+		CONSTRAINT PK_channel_id PRIMARY KEY CLUSTERED (id),
+		name VARCHAR(100) NOT NULL,
+		date DATE NOT NULL,
+		follower INT NOT NULL,
+		monetization_id INT FOREIGN KEY REFERENCES Moukitube.dbo.monetization(id),
+		CONSTRAINT Uniqueplace_channel UNIQUE (name, date, follower)
+	)
 
-ALTER DATABASE [MoukiTube] SET ANSI_PADDING OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.storie
+	Create table Moukitube.dbo.storie(
+		id INT NOT NULL,
+		CONSTRAINT PK_storie_id PRIMARY KEY CLUSTERED (id),
+		channelname VARCHAR(100) NOT NULL,
+		date DATE NULL,
+		time TIME NULL,
+		channel_id INT FOREIGN KEY REFERENCES Moukitube.dbo.channel(id),
+		CONSTRAINT Uniqueplace_storie UNIQUE (channelname, date, time)
+	)
 
-ALTER DATABASE [MoukiTube] SET ANSI_WARNINGS OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.channel_share_storie
+	Create table Moukitube.dbo.channel_share_storie(
+		id INT NOT NULL,
+		CONSTRAINT PK_channel_share_storie_id PRIMARY KEY CLUSTERED (id),
+		channel_id INT FOREIGN KEY REFERENCES Moukitube.dbo.channel(id),
+		storie_id INT FOREIGN KEY REFERENCES Moukitube.dbo.storie(id),
+	)
 
-ALTER DATABASE [MoukiTube] SET ARITHABORT OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.users
+	Create table Moukitube.dbo.users(
+		id INT NOT NULL,
+		CONSTRAINT PK_users_id PRIMARY KEY CLUSTERED (id),
+		name VARCHAR(100) NOT NULL,
+		firstname VARCHAR(100) NOT NULL,
+		email VARCHAR(100) NOT NULL,
+		password VARCHAR(48) NOT NULL,	
+		date DATE NULL,
+		channel_id INT FOREIGN KEY REFERENCES Moukitube.dbo.channel(id),
+		subscriptions_id INT FOREIGN KEY REFERENCES Moukitube.dbo.subscriptions(id),
+		CONSTRAINT Uniqueplace_users UNIQUE (name, firstname, email, password)
+	)
 
-ALTER DATABASE [MoukiTube] SET AUTO_CLOSE OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.types
+	Create table Moukitube.dbo.types(
+		id INT NOT NULL,
+		CONSTRAINT PK_types_id PRIMARY KEY CLUSTERED (id),
+		name VARCHAR (30) NOT NULL,
+		CONSTRAINT Uniqueplace_types UNIQUE (name)
+	)
 
-ALTER DATABASE [MoukiTube] SET AUTO_SHRINK OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.videos
+	Create table Moukitube.dbo.videos(
+		id INT NOT NULL,
+		CONSTRAINT PK_videos_id PRIMARY KEY CLUSTERED (id),
+		title VARCHAR(100) NOT NULL,
+		channelname VARCHAR(100) NOT NULL,
+		date DATE NOT NULL,
+		viewes VARCHAR(10) NOT NULL,										/* Fornat décimal	*/
+		types_id INT FOREIGN KEY REFERENCES Moukitube.dbo.types(id)
+		CONSTRAINT Uniqueplace_videos UNIQUE (title, channelname)
+	)
 
-ALTER DATABASE [MoukiTube] SET AUTO_UPDATE_STATISTICS ON 
 GO
+	DROP TABLE IF exists Moukitube.dbo.comments
+	Create table Moukitube.dbo.comments(
+		id INT NOT NULL,
+		CONSTRAINT PK_comments_id PRIMARY KEY CLUSTERED (id),
+		channelname VARCHAR(100) NOT NULL,
+		description VARCHAR(100) NOT NULL,
+		videos_id INT FOREIGN KEY REFERENCES Moukitube.dbo.videos(id)
+		CONSTRAINT Uniqueplace_comments UNIQUE (channelname)
+	)
 
-ALTER DATABASE [MoukiTube] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.sponsor
+	Create table Moukitube.dbo.sponsor(
+		id INT NOT NULL,
+		CONSTRAINT PK_sponsor_id PRIMARY KEY CLUSTERED (id),
+		name VARCHAR (100) NOT NULL,
+		CONSTRAINT Uniquesponsorr UNIQUE (Name)
+	)
 
-ALTER DATABASE [MoukiTube] SET CURSOR_DEFAULT  GLOBAL 
 GO
+	DROP TABLE IF exists Moukitube.dbo.videos_sponsorised_sponsors
+	Create table Moukitube.dbo.videos_sponsorised_sponsors(
+		id INT NOT NULL,
+		CONSTRAINT PK_videos_sponsorised_sponsors_id PRIMARY KEY CLUSTERED (id),
+		price FLOAT NULL,
+		videos_id INT FOREIGN KEY REFERENCES Moukitube.dbo.videos(id),
+		sponsor_id INT FOREIGN KEY REFERENCES Moukitube.dbo.sponsor(id),
+		CONSTRAINT Uniquevideos_sponsorised_sponsors UNIQUE (price)
+	)
 
-ALTER DATABASE [MoukiTube] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.videos_like_users
+	Create table Moukitube.dbo.videos_like_users(
+		id INT NOT NULL,
+		CONSTRAINT PK_videos_like_users_id PRIMARY KEY CLUSTERED (id),
+		types VARCHAR(100) NULL,
+		videos_id INT FOREIGN KEY REFERENCES Moukitube.dbo.videos(id),
+		users_id INT FOREIGN KEY REFERENCES Moukitube.dbo.users(id),
+	)
 
-ALTER DATABASE [MoukiTube] SET NUMERIC_ROUNDABORT OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.videos_share_users
+	Create table Moukitube.dbo.videos_share_users(
+		id INT NOT NULL,
+		CONSTRAINT PK_videos_share_users_id PRIMARY KEY CLUSTERED (id),
+		videos_id INT FOREIGN KEY REFERENCES Moukitube.dbo.videos(id),
+		users_id INT FOREIGN KEY REFERENCES Moukitube.dbo.users(id),
+	)
 
-ALTER DATABASE [MoukiTube] SET QUOTED_IDENTIFIER OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.playlists
+	Create table Moukitube.dbo.playlists(
+		id INT NOT NULL,
+		CONSTRAINT PK_playlists_id PRIMARY KEY CLUSTERED (id),
+		name VARCHAR (50) NOT NULL,
+		channel VARCHAR(100) NOT NULL,
+		types_id INT FOREIGN KEY REFERENCES Moukitube.dbo.types(id),
+		users_id INT FOREIGN KEY REFERENCES Moukitube.dbo.users(id),
+	)
 
-ALTER DATABASE [MoukiTube] SET RECURSIVE_TRIGGERS OFF 
 GO
+	DROP TABLE IF exists Moukitube.dbo.playlists_read_users
+	Create table Moukitube.dbo.playlists_read_users(
+		id INT NOT NULL,
+		CONSTRAINT PK_playlists_read_users_id PRIMARY KEY CLUSTERED (id),
+		playlists_id INT FOREIGN KEY REFERENCES Moukitube.dbo.playlists(id),
+		users_id INT FOREIGN KEY REFERENCES Moukitube.dbo.users(id),
+	)
 
-ALTER DATABASE [MoukiTube] SET  DISABLE_BROKER 
 GO
-
-ALTER DATABASE [MoukiTube] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET TRUSTWORTHY OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET PARAMETERIZATION SIMPLE 
-GO
-
-ALTER DATABASE [MoukiTube] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET HONOR_BROKER_PRIORITY OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET RECOVERY FULL 
-GO
-
-ALTER DATABASE [MoukiTube] SET  MULTI_USER 
-GO
-
-ALTER DATABASE [MoukiTube] SET PAGE_VERIFY CHECKSUM  
-GO
-
-ALTER DATABASE [MoukiTube] SET DB_CHAINING OFF 
-GO
-
-ALTER DATABASE [MoukiTube] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-
-ALTER DATABASE [MoukiTube] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-
-ALTER DATABASE [MoukiTube] SET DELAYED_DURABILITY = DISABLED 
-GO
-
-ALTER DATABASE [MoukiTube] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-
-ALTER DATABASE [MoukiTube] SET QUERY_STORE = OFF
-GO
-
-ALTER DATABASE [MoukiTube] SET  READ_WRITE 
-GO
-
+	DROP TABLE IF exists Moukitube.dbo.storie_like_channel
+	Create table Moukitube.dbo.stories_like_channel(
+		id INT NOT NULL,
+		CONSTRAINT PK_stories_like_channel_id PRIMARY KEY CLUSTERED (id),
+		types VARCHAR (100) NOT NULL,
+		storie_id INT FOREIGN KEY REFERENCES Moukitube.dbo.storie(id),
+		channel_id INT FOREIGN KEY REFERENCES Moukitube.dbo.channel(id),
+	)
