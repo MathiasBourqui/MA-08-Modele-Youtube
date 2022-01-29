bd288e3d5f69c82529a03e93ddba3e0d43c5a0df
Paul-LoupGermain <Paul-LOUP.GERMAIN@cpnv.ch>
2022-01-29 14:22:36 +0100

    WIP

--- Rendu/SelectDatabase MoukiTube.sql
+++ Rendu/SelectDatabase MoukiTube.sql
@@ -3,3 +3,41 @@
 	Date : 27/01/2022
 	Projet : Création du fichier permettant de vérifier la cohérence de l'insertion des données
 */
+
+USE Moukitube
+
+/* Table subscriptions */
+SELECT DISTINCT types FROM subscriptions;
+
+/* Table monetization */
+SELECT amount FROM monetization
+ORDER BY amount;
+
+/* Table channel */
+SELECT date FROM channel
+WHERE date LIKE ('%2010%');
+
+/* Table users */
+SELECT email FROM users
+WHERE email LIKE ('%yahoo%');
+
+/* Table types */
+SELECT name FROM types
+WHERE name IN ('Non-répertorié');
+
+/* Table video */
+SELECT date FROM videos
+WHERE date LIKE ('%2015%');
+
+/* Table sponsor */
+SELECT name, count(name) AS nb FROM sponsor
+GROUP BY name;
+
+/* Table videos_sponsorised_sponsors */
+SELECT price FROM videos_sponsorised_sponsors
+ORDER BY price DESC;
+
+/* Table videos_like_users */
+SELECT types, count(types) AS total FROM videos_like_users
+GROUP BY types;
+
--- Rendu/ViewsDatabase MoukiTube.sql
+++ Rendu/ViewsDatabase MoukiTube.sql
@@ -3,3 +3,20 @@
 	Date : 27/01/2022
 	Projet : Création du fichier permettant lister le nombre d'enregistrement d'une table
 */
+
+USE Moukitube
+
+SELECT COUNT(*) AS subscriptions FROM subscriptions;
+SELECT COUNT(*) AS monetization FROM monetization;
+SELECT COUNT(*) AS channel FROM channel;
+SELECT COUNT(*) AS users FROM users;
+SELECT COUNT(*) AS types FROM types;
+SELECT COUNT(*) AS videos FROM videos;
+SELECT COUNT(*) AS comments FROM comments;
+SELECT COUNT(*) AS sponsor FROM sponsor;
+SELECT COUNT(*) AS playlists FROM playlists;
+SELECT COUNT(*) AS videos_sponsorised_sponsors FROM videos_sponsorised_sponsors;
+SELECT COUNT(*) AS videos_like_users FROM videos_like_users;
+SELECT COUNT(*) AS videos_share_users FROM videos_share_users;
+SELECT COUNT(*) AS playlists_read_users FROM playlists_read_users;
+SELECT COUNT(*) AS users_subscribe_channel FROM users_subscribe_channel;
